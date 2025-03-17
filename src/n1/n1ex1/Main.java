2e6b8652587a0c1c610ff606bf3184085c598f92
zohra-b <z.bellamine@gmail.com>
2025-03-17 20:44:28 +0100

    S103 n1ex1 finished

--- src/n1/n1ex1/Main.java
+++ src/n1/n1ex1/Main.java
@@ -1,6 +1,8 @@
 package n1.n1ex1;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,10 +19,33 @@ public class Main {
         months.add(new Month("November"));
         months.add(new Month("December"));
 
-      months.set(7, new Month("August"));
+        months.add(7, new Month("August"));
 
-      for (Month m : months){
-          System.out.println(m);
-      }
+        for (Month m : months) {
+            System.out.println(m);
+        }
+
+        System.out.println("\nHASH SET CREATED USING CONSTRUCTOR : ");
+        HashSet<Month> HashThroughConstructor = new HashSet<>(months);
+        for (Month m : HashThroughConstructor) {
+            System.out.println(m);
+        }
+
+        System.out.println("\nHash set adding january : ");
+        HashThroughConstructor.add(new Month("January"));
+        for (Month m : HashThroughConstructor) {
+            System.out.println(m);
+        }
+
+        System.out.println("\nUSING ITERATOR : ");
+        Iterator<Month> iterator = HashThroughConstructor.iterator();
+        while (iterator.hasNext()){
+            Month month = iterator.next();
+            System.out.println(month);
+        }
+
+
+
+
     }
 }
--- src/n1/n1ex1/Month.java
+++ src/n1/n1ex1/Month.java
@@ -1,11 +1,32 @@
 package n1.n1ex1;
 
+import java.util.Objects;
+
 public class Month {
     private String name;
 
     public Month(String name){
         this.name = name;
     }
+
+    public String getName(){
+    return this.name;
+    }
+
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Month month = (Month) obj;
+        return Objects.equals(this.name, ((Month) obj).name);
+    }
+
+   @Override
+   public int hashCode(){
+       return Objects.hash(name);
+   }
 
     @Override
     public String toString(){
